052dddf177731149d8b9fe8c2870a366f7d2e3f6
Oleksandra Tovshchyk <138442454+sashatov@users.noreply.github.com>
2023-09-06 11:21:19 +0200

    Revert "fixed some little things"

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,6 +1,4 @@
 class ThemesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:update]
-
   def update
     cookies[:theme] = params[:theme]
     redirect_to(request.referrer || root_path)
